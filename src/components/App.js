44c0fb688a717866977dd6ee1c36847e2f12df8a
Alexander Vikenfalk <vikenfalk@business-keeper.local>
2021-03-13 15:59:21 +0100

    Added SCSS for the background.

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Background from "./Background"
+
+function App() {
+    return (
+        <div className="app">
+            <Background/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Background.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Background() {
+    return (
+        <div className="background">
+            <div className="background__stars--small"/>
+            <div className="background__stars--medium"/>
+            <div className="background__stars--large"/>
+        </div>
+    );
+}
+
+export default Background;
